9fb8cee5f5d07cfa2597c8662d02099ca9058508
ft-perfect <themightyomegagd@gmail.com>
2023-09-30 18:41:45 +0700

    bugfix

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-		int input;
+    public static void main(String[] args){
         boolean exit = false;
+		int input;
 
         while(!exit){
             input = home();
+			switch(input){
+				case 1:
+				
+			}
         }
     }
 
 	public static int home(){
-		System.out.println("MENU : \n1. Sistem Persamaan Linier\n2. Determinan\n3. Matriks Balikan\n4. Interpolasi Polinom\n5. Interpolasi Bicubic Spine\n6. Regresi Linier Berganda\n7.Keluar\nMasukkan pilihan :");
+		System.out.println("MENU : \n1. Sistem Persamaan Linier\n2. Determinan\n3. Matriks Balikan\n4. Interpolasi Polinom\n5. Interpolasi Bicubic Spine\n6. Regresi Linier Berganda\n7.Keluar\nMasukkan pilihan(1-7):");
 
-		
 		int input;
+		input = cin();
 		
-		while(in)
+		while(input < 1 || 7 < input){
+			System.out.println("Input tidak valid.\nMasukkan pilihan(1-7):");
+			input = cin();
+		}
 
+		return input;
 	}
 
 	public static int cin(){
