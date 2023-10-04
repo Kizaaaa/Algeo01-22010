ff747aa8f3da0c42a786b0067a3a59b9b6eed9ed
Flora <mariaflorarenata12@gmail.com>
2023-10-05 01:01:40 +0700

    finalize interpolations, add gaussNoText, add open/close message to main

--- src/PolynomialInterpolation.java
+++ src/PolynomialInterpolation.java
@@ -3,26 +3,55 @@ import java.lang.Math;
 
 public class PolynomialInterpolation{
     public static void interpolation(Matrix m, float x, Scanner sc){
-        //nanti bikin gauss no text version, sekarang flora turu dahulu
         float result = 0;
-        float[] aVals=Gauss.f(m, sc);
-        System.out.print("f(x)=");
-        for (int i=aVals.length-1;i>=0;i--){
-            System.out.print(aVals[i]+"x^"+i+" + ");
-            result+=aVals[i]*Math.pow(x,i);
+        float[] aVals=Gauss.gaussNoText(m, sc);
+        float[] failRet={6,9}; //dari Gauss
+        if (aVals==failRet){
+            System.out.println("Maaf, titik-titik tersebut tidak bisa diinterpolasi");
         }
-        System.out.print(", f("+x+")=");
-        System.out.print(result+"\n");
+        else{
+            String outString;
+            outString="f(x) = ";
+            for (int i=aVals.length-1;i>0;i--){
+                outString+=(Float.toString(aVals[i]));
+                outString+=("x^"); 
+                outString+=(Integer.toString(i));
+                outString+=(" + ");
+                result+=aVals[i]*Math.pow(x,i);
+            }
+            result+=aVals[0];
+            outString+=(Float.toString(aVals[0]));
+            outString+=(", f(");
+            outString+=(Float.toString(x));
+            outString+=(") = ");
+            outString+=(Float.toString(result));
+            System.out.println(outString);
+            System.out.print("Tulis hasil dalam file .txt? (y/n): ");
+            String txt = sc.next();
+            while(!txt.equals("y") && !txt.equals("Y") && !txt.equals("n") && !txt.equals("N")){
+                System.out.print("Input tidak valid, silahkan input kembali: ");
+                txt = sc.next();
+            }
+            if(txt.equals("y") || txt.equals("Y")){
+                TXTReaderWriter.writeTXT(sc, outString);
+            }
+        }
     }
 
     public static Matrix keyInput(Scanner sc){
-        System.out.println("Masukkan jumlah titik yang akan diinterpolasi (minimal 1): ");            
-        int n = Main.cinMinCheck(sc, 1);
+        System.out.println("Masukkan jumlah titik yang akan diinterpolasi (minimal 2): ");            
+        int n = Main.cinMinCheck(sc, 2);
         sc.nextLine();
         Matrix m = new Matrix(n, n+1);
         System.out.println("Masukkan "+n+" titik: \n(Format: (x0,y0), (x1,y1), ..., (xn,yn))");
         String line=sc.nextLine(); 
         String[] points=line.split(", ");
+        while(points.length!=n){
+            System.out.println("Input tidak valid, silahkan input kembali: ");
+            System.out.println("Masukkan "+n+" titik: \n(Format: (x0,y0), (x1,y1), ..., (xn,yn))");
+            line=sc.nextLine(); 
+            points=line.split(", ");
+        }
         for (int i=0;i<m.row;i++){
             String[] xArr=points[i].split("[\\(||\\)||,]");
             Double x=Double.parseDouble(xArr[1]);
@@ -32,22 +61,6 @@ public class PolynomialInterpolation{
             m.set(i,n,Float.parseFloat(xArr[2]));
         }
         return m;
-    }
-
-    public static Matrix fileInput(float taksiran, Scanner sc){
-        Matrix m = TXTReaderWriter.readTXT(sc);
-        Matrix mret = new Matrix(m.row-1, m.row);
-        for (int i=0;i<mret.row;i++){
-            float x=m.element[i][0];
-            for (int j=0;j<mret.row;j++){
-                mret.set(i,j,(float)Math.pow(x,j));
-            }
-            mret.set(i,mret.col-1,m.element[i][1]);
-        }
-        m.displayMatrix();
-        mret.displayMatrix();
-        taksiran=m.elmt(m.row-1, 0);
-        return mret;
     }
 
     public static void main(Scanner sc){
@@ -61,9 +74,17 @@ public class PolynomialInterpolation{
             taksiran=sc.nextFloat();
         }
         else{
-            interpolationMatrix=fileInput(taksiran,sc);}
-        interpolationMatrix.displayMatrix();
-        System.out.println("\ntaksiran:" + taksiran);
+            Matrix m = TXTReaderWriter.readTXT(sc);
+            interpolationMatrix = new Matrix(m.row-1, m.row);
+            for (int i=0;i<interpolationMatrix.row;i++){
+                float x=m.element[i][0];
+                for (int j=0;j<interpolationMatrix.row;j++){
+                    interpolationMatrix.set(i,j,(float)Math.pow(x,j));
+                }
+                interpolationMatrix.set(i,interpolationMatrix.col-1,m.element[i][1]);
+            }
+            taksiran=m.elmt(m.row-1, 0);
+        }
         interpolation(interpolationMatrix,taksiran,sc);
     }
 
